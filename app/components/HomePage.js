f7086c476c3a1a54557ab8490b59597020bdb8e5
Amneet Sandhu <amneet954@gmail.com>
2020-05-27 21:33:50 -0400

    Created CSS Grid Design on HomePage. Test for AllCases.js

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -24,13 +24,24 @@ class HomePage extends Component {
       );
     } else {
       return (
-        <div className="cardContainer">
-          <div className="card">
-            <h2>
-              Salesforce Instance:{" "}
-              {this.props.domainlist.domain.salesforceInstance}
-            </h2>
-          </div>
+        // <div className="cardContainer">
+        //   <div className="card">
+        //     <h2>
+        //       Salesforce Instance:{" "}
+        //       {this.props.domainlist.domain.salesforceInstance}
+        //     </h2>
+        //   </div>
+        // </div>
+        <div className="grid-1">
+          <div className="item-1">1</div>
+          <div className="item-2">2</div>
+          <div className="item-3">3</div>
+          <div className="item-4">4</div>
+          <div className="item-5">5</div>
+          <div className="item-6">6</div>
+          <div className="item-7">7</div>
+          <div className="item-8">8</div>
+          <div className="item-9">9</div>
         </div>
       );
     }
